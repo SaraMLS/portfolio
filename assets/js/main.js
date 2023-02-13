7714192df8abdcceb9a969839c5957fd041073ba
SaraMLS <saramelais57@gmail.com>
2023-02-13 20:57:48 +0100

    menu burger + homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const burger = document.querySelector(".burger");
+const navbar = document.querySelector("nav");
 let navigation = document.querySelector(".navigation")
 
 burger.addEventListener("click", () => {
@@ -10,5 +11,19 @@ burger.addEventListener("click", () => {
 	} else {
 		navigation.classList.remove("hidden")
 		burger.classList.add("active")
+	}
+})
+
+let burgerRect = burger.getBoundingClientRect()
+let burgerHeight = burgerRect.top + burgerRect.height
+
+document.addEventListener("scroll", () => {
+	let className = "scrolled"
+	if (window.scrollY >= (burgerHeight - 20)) {
+		navbar.classList.add(className)
+		burger.classList.add(className)
+	} else {
+		navbar.classList.remove(className)
+		burger.classList.remove(className)
 	}
 })
